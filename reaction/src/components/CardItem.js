43ff6f9c8fc971375ead8fcfb480ce1f2906bc34
Mary Ann Tapiru <maryann.baricante24@gmail.com>
2022-09-25 21:13:10 +0800

    Update 3

--- reaction/src/components/CardItem.js
+++ reaction/src/components/CardItem.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
         <div>
             <li className="cards__item">
-                <Link className='cards__item__link'>
-                    <figure className='cards_item_pic-wrap'>
-                        <img src="/" alt="Travel Image" className="cards__item__img" />
+                <Link className='cards__item__link' to={props.path}>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <img src={props.src} alt={props.text} className="cards__item__img" />
                     </figure>
-                    <div className="cards_item_info">
-                        <h5 className="cards__item_text">
-                            
-                        </h5>
+                    <div className="cards__item__info">
+                        <h5 className="cards__item__text">{props.text}</h5>
                     </div>
                 </Link>
             </li>
--- reaction/src/components/Cards.js
+++ reaction/src/components/Cards.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
   return (
-    <div>Cards</div>
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem 
+            src='images/img-9.jpg'
+            text="Explore the hidden waterfall deep inside the Amazon Jungle"
+            label='Adventure'
+            path='/services'
+            />
+            <CardItem 
+            src='images/img-2.jpg'
+            text="Travel joyfully through the Islands of Bali in a Private Cruise"
+            label='Happy'
+            path='/services'
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem 
+            src='images/img-1.jpg'
+            text="Enjoy your tour in this places and have fun."
+            label='Adventure'
+            path='/services'
+            />
+            <CardItem 
+            src='images/img-3.jpg'
+            text="Enjoy your tour in this places and have fun."
+            label='Happy'
+            path='/services'
+            />
+            <CardItem 
+            src='images/img-4.jpg'
+            text="Enjoy your tour in this places and have fun."
+            label='Happy'
+            path='/services'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Cards
+export default Cards;
